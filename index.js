7817a87bfe1c5d5d4b75764535beaf0660f6e6f0
ebrahimmfadae <ebrahimmfadae@gmail.com>
2023-02-09 19:26:49 +0330

    Code cleanup

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { privateKey, publicKey } = await promisify(crypto.generateKeyPair)(
 console.log(privateKey.export({ format: "pem", type: "pkcs1" }));
 console.log(publicKey.export({ format: "pem", type: "pkcs1" }));
 
-const cipher = privateEncrypt(
-  privateKey,
-  Buffer.from("This message is classified")
-);
+const message = Buffer.from("This message is classified");
+
+const cipher = privateEncrypt(privateKey, message);
 const cipherBase64 = cipher.toString("base64");
 console.log("Encrypted message:", `\x1b[31m${cipherBase64}\x1b[0m`);
+
 const plain = publicDecrypt(publicKey, Buffer.from(cipherBase64, "base64"));
 console.log("Decrypted message:", `\x1b[32m${plain.toString("ascii")}\x1b[0m`);
